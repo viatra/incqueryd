96ec59120ba0cff39b434d83903b189ff4ddc012
Gabor Szarnyas <szarnyasg@gmail.com>
2013-10-10 09:26:18 +0000

    inital commit of 4store control benchmark project
    git-svn-id: https://svn.inf.mit.bme.hu/projects/ontology/source/incqueryd@890 25365000-b138-4714-a2de-83ea935fac61

---
+++ incqueryd-4s-control/src/main/java/fourstore/Worker.java
@@ -0,0 +1,55 @@
+package fourstore;
+
+import hu.bme.mit.train.util.BenchmarkResult;
+
+import java.io.IOException;
+
+public class Worker {
+
+	protected final FourStoreClient client;
+	protected final int runCount;
+
+	public Worker(final String serverUrl, final String filename, final int runCount) {
+		client = new FourStoreClient(serverUrl, filename);
+		this.runCount = runCount;
+	}
+
+	public void work() throws IOException, InterruptedException {
+
+		BenchmarkResult.INSTANCE.startStopper();
+		BenchmarkResult.INSTANCE.setId("TestCaseRouteSensor_4store-control");
+
+		// load
+		System.out.println("Load");
+		client.load();
+		BenchmarkResult.INSTANCE.setReadTime();
+		BenchmarkResult.INSTANCE.startStopper();
+		
+		// check 1
+		System.out.println();
+		System.out.println("Check 1");
+		final long count1 = client.check();
+		BenchmarkResult.INSTANCE.addCheckTime();
+		BenchmarkResult.INSTANCE.addInvalid(count1);
+		
+		for (int i = 0; i < runCount; i++) {
+			// edit
+			System.out.println();
+			System.out.println("Edit");
+			client.edit();
+			BenchmarkResult.INSTANCE.addEditTime();
+			BenchmarkResult.INSTANCE.startStopper();
+	
+			// check 2
+			System.out.println();
+			System.out.println("Check 2");
+			final long count2 = client.check();
+			BenchmarkResult.INSTANCE.addCheckTime();
+			BenchmarkResult.INSTANCE.addInvalid(count2);
+		}
+		
+		System.out.println(BenchmarkResult.INSTANCE.toString());
+		
+	}
+
+}
---
+++ incqueryd-4s-control/src/main/java/hu/bme/mit/train/util/OsgiCc.java
@@ -0,0 +1,5 @@
+package hu.bme.mit.train.util;
+
+public interface OsgiCc {
+	public void runBenchmarks();
+}
