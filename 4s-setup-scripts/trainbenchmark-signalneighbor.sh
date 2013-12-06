49161518ce37a4871abec4957c3d29fec91b5cbd
Gabor Szarnyas <szarnyasg@gmail.com>
2013-12-06 17:19:49 +0000

    TB queries added
    git-svn-id: https://svn.inf.mit.bme.hu/projects/ontology/source/incqueryd@1094 25365000-b138-4714-a2de-83ea935fac61

---
+++ 4s-setup-scripts/trainbenchmark-signalneighbor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo The SignalNeighbor query will return 0 as 4store does not support type inferencing.
+. trainbenchmark-query.sh SignalNeighbor
---
+++ 4s-setup-scripts/trainbenchmark.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. trainbenchmark-poslength.sh
+. trainbenchmark-routesensor.sh
+. trainbenchmark-signalneighbor.sh
+. trainbenchmark-switchsensor.sh
