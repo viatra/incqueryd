7e890412ab878c09702fdc3193f4a730ca5ae7b6
Gabor Szarnyas <szarnyasg@gmail.com>
2014-01-03 22:54:15 +0100

    added Sapphire project for architecture dependency viewer/editor

---
+++ sapphire/org.eclipse.sapphire.samples.architecture/src/org/eclipse/sapphire/samples/architecture/ConnectionBendpoint.java
@@ -0,0 +1,54 @@
+/******************************************************************************
+ * Copyright (c) 2013 Oracle
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Shenxue Zhou - initial implementation
+ ******************************************************************************/
+
+package org.eclipse.sapphire.samples.architecture;
+
+import org.eclipse.sapphire.Element;
+import org.eclipse.sapphire.ElementType;
+import org.eclipse.sapphire.Value;
+import org.eclipse.sapphire.ValueProperty;
+import org.eclipse.sapphire.modeling.annotations.DefaultValue;
+import org.eclipse.sapphire.modeling.annotations.Type;
+import org.eclipse.sapphire.modeling.xml.annotations.XmlBinding;
+
+/**
+ * @author <a href="mailto:shenxue.zhou@oracle.com">Shenxue Zhou</a>
+ */
+
+public interface ConnectionBendpoint extends Element 
+{
+	ElementType TYPE = new ElementType( ConnectionBendpoint.class );
+	
+    // *** X ***
+    
+    @Type( base = Integer.class )
+    @XmlBinding( path = "@x" )
+    @DefaultValue( text = "0" )
+    
+    ValueProperty PROP_X = new ValueProperty( TYPE, "X");
+    
+    Value<Integer> getX();
+    void setX(Integer value);
+    void setX(String value);
+
+    // *** Y ***
+    
+    @Type( base = Integer.class )
+    @XmlBinding( path = "@y" )
+    @DefaultValue( text = "0" )
+
+    ValueProperty PROP_Y = new ValueProperty( TYPE, "Y");
+    
+    Value<Integer> getY();
+    void setY(Integer value);
+    void setY(String value);
+	
+}
