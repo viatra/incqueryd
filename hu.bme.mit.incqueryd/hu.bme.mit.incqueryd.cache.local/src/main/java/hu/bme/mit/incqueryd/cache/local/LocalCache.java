ae7570d92103973a1486a3f6caa575e0d4242d48
Dénes Harmath <harmathdenes@gmail.com>
2014-02-03 17:10:59 +0100

    Fix local cache implementation

--- hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.cache.local/src/main/java/hu/bme/mit/incqueryd/cache/local/LocalCache.java
+++ hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.cache.local/src/main/java/hu/bme/mit/incqueryd/cache/local/LocalCache.java
@@ -3,12 +3,24 @@ package hu.bme.mit.incqueryd.cache.local;
 import hu.bme.mit.incqueryd.cache.DistributedCache;
 import hu.bme.mit.incqueryd.cache.DistributedMultiMap;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import com.google.common.collect.HashMultimap;
 
 public class LocalCache implements DistributedCache {
 
+	private final LoadingCache<String, DistributedMultiMap<Object, Object>> multiMaps = CacheBuilder.newBuilder().build(new CacheLoader<String, DistributedMultiMap<Object, Object>>() {
+
+		@Override
+		public DistributedMultiMap<Object, Object> load(String key) throws Exception {
+			return new LocalMultiMap<>(HashMultimap.create());
+		}
+
+	});
+
 	public <K, V> DistributedMultiMap<K, V> getMultiMap(String name) {
-		return new LocalMultiMap<K, V>(HashMultimap.<K, V> create());
+		return (DistributedMultiMap<K, V>) multiMaps.getUnchecked(name);
 	}
 
 }
