d4a29680df10abab46784e41b1a5c2eb15360919
Gabor Szarnyas <szarnyasg@gmail.com>
2014-02-14 10:29:33 +0100

    serializer works from string and to string

--- hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/util/RecipeSerializer.java
+++ hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/util/RecipeSerializer.java
@@ -1,15 +1,16 @@
 package hu.bme.mit.incqueryd.util;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.io.FileUtils;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.eclipse.incquery.runtime.rete.recipes.RecipesPackage;
 import org.eclipselabs.emfjson.EMFJs;
 import org.eclipselabs.emfjson.resource.JsResourceFactoryImpl;
 
@@ -36,13 +37,28 @@ public class RecipeSerializer {
 		resource.save(options);
 	}
 
-	public static EObject deserialize(String filename) throws IOException {
+	public static String serializeToString(final EObject eObject) throws IOException {
+		final File tempFile = File.createTempFile("recipe-", ".json");
+		serialize(eObject, tempFile.getAbsolutePath());
+		final String modelString = FileUtils.readFileToString(tempFile);
+
+		return modelString;
+	}
+
+	public static EObject deserialize(final String filename) throws IOException {
 		final ResourceSet resourceSet = init();
-		final Map<String, Object> options = new HashMap<String, Object>();
-		options.put(EMFJs.OPTION_ROOT_ELEMENT, RecipesPackage.eINSTANCE.getTrimmerRecipe());
+		final Map<String, Object> options = new HashMap<String, Object>();		
 		final Resource resource = resourceSet.createResource(URI.createURI(filename));
 		resource.load(options);
+		
 		return resource.getContents().get(0);
+	}
+
+	public static EObject deserializeFromString(final String model) throws IOException {
+		final File tempFile = File.createTempFile("recipe-", ".json");
+		FileUtils.writeStringToFile(tempFile, model);
+		
+		return deserialize(tempFile.getPath());
 	}
 
 }
--- hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.core/src/test/java/hu/bme/mit/incqueryd/recipes/JsonSerializerTest.java
+++ hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.core/src/test/java/hu/bme/mit/incqueryd/recipes/JsonSerializerTest.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.incquery.runtime.rete.recipes.BetaRecipe;
+import org.eclipse.incquery.runtime.rete.recipes.JoinRecipe;
 import org.eclipse.incquery.runtime.rete.recipes.Mask;
 import org.eclipse.incquery.runtime.rete.recipes.ProjectionIndexer;
 import org.eclipse.incquery.runtime.rete.recipes.RecipesFactory;
@@ -14,16 +15,16 @@ import org.junit.Test;
 
 public class JsonSerializerTest {
 
+	private static final EObject JoinRecipe = null;
+
 	@Test
 	public void serializeTest() throws IOException {
-		System.out.println("Serialize.");
-
 		final BetaRecipe recipe = RecipesFactory.eINSTANCE.createJoinRecipe();
 		final Mask primaryMask = RecipesFactory.eINSTANCE.createMask();
 		primaryMask.getSourceIndices().addAll(Arrays.asList(2));
 		final Mask secondaryMask = RecipesFactory.eINSTANCE.createMask();
 		secondaryMask.getSourceIndices().addAll(Arrays.asList(0));
-		
+
 		final ProjectionIndexer primaryParent = RecipesFactory.eINSTANCE.createProjectionIndexer();
 		primaryParent.setMask(primaryMask);
 		recipe.setLeftParent(primaryParent);
@@ -31,16 +32,52 @@ public class JsonSerializerTest {
 		final ProjectionIndexer secondaryParent = RecipesFactory.eINSTANCE.createProjectionIndexer();
 		secondaryParent.setMask(secondaryMask);
 		recipe.setRightParent(secondaryParent);
-		
+
 		RecipeSerializer.serialize(recipe, "model.json");
 	}
 
 	@Test
 	public void deserialize() throws IOException {
-		System.out.println("Deserialize.");
-
 		final EObject eObject = RecipeSerializer.deserialize("model.json");
 		System.out.println(eObject);
+	}
+
+	@Test
+	public void serializeToStringTest() throws IOException {
+		final BetaRecipe recipe = RecipesFactory.eINSTANCE.createJoinRecipe();
+		final Mask primaryMask = RecipesFactory.eINSTANCE.createMask();
+		primaryMask.getSourceIndices().addAll(Arrays.asList(2));
+		final Mask secondaryMask = RecipesFactory.eINSTANCE.createMask();
+		secondaryMask.getSourceIndices().addAll(Arrays.asList(0));
+
+		final ProjectionIndexer primaryParent = RecipesFactory.eINSTANCE.createProjectionIndexer();
+		primaryParent.setMask(primaryMask);
+		recipe.setLeftParent(primaryParent);
+
+		final ProjectionIndexer secondaryParent = RecipesFactory.eINSTANCE.createProjectionIndexer();
+		secondaryParent.setMask(secondaryMask);
+		recipe.setRightParent(secondaryParent);
+
+		final String model = RecipeSerializer.serializeToString(recipe);
+		System.out.println(model);
+	}
+
+	@Test
+	public void deserializeFromStringTest() throws IOException {
+		final String model = "{\n" + "  \"eClass\" : \"http://www.eclipse.org/incquery/rete/recipes#//JoinRecipe\",\n"
+				+ "  \"leftParent\" : {\n"
+				+ "    \"eClass\" : \"http://www.eclipse.org/incquery/rete/recipes#//ProjectionIndexer\",\n"
+				+ "    \"mask\" : {\n"
+				+ "      \"eClass\" : \"http://www.eclipse.org/incquery/rete/recipes#//Mask\",\n"
+				+ "      \"sourceIndices\" : [ 2 ]\n" + "    }\n" + "  },\n" + "  \"rightParent\" : {\n"
+				+ "    \"eClass\" : \"http://www.eclipse.org/incquery/rete/recipes#//ProjectionIndexer\",\n"
+				+ "    \"mask\" : {\n"
+				+ "      \"eClass\" : \"http://www.eclipse.org/incquery/rete/recipes#//Mask\",\n"
+				+ "      \"sourceIndices\" : [ 0 ]\n" + "    }\n" + "  }\n" + "}";
+		final EObject eObject = RecipeSerializer.deserializeFromString(model);
+		final JoinRecipe recipe =  (JoinRecipe) eObject;
+		System.out.println(recipe.getLeftParent().getMask().getSourceIndices());
+		System.out.println(recipe.getRightParent().getMask().getSourceIndices());
 	}
 
 }
