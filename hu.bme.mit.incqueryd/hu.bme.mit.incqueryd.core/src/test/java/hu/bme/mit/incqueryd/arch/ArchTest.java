07d6063c71915190c6c55863077dc9ab5a718523
Gabor Szarnyas <szarnyasg@gmail.com>
2014-02-15 23:30:35 +0100

    added EMF, Ecore and Ecore-XMI dependencies to the IQD core

---
+++ hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.core/src/test/java/hu/bme/mit/incqueryd/arch/ArchTest.java
@@ -0,0 +1,35 @@
+package hu.bme.mit.incqueryd.arch;
+
+import org.junit.Test;
+//import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
+
+public class ArchTest {
+
+	@Test
+	public void test() {
+
+//		final Map<ReteNodeRecipe, String> names = new HashMap<>();
+//		final Map<ReteNodeRecipe, ReteNodeRecipe> children = new HashMap<>();
+//		final Map<ReteNodeRecipe, JoinSide> childrenSides = new HashMap<>();
+//
+//		// initialize extension to factory map
+//		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("arch", new XMIResourceFactoryImpl());
+//		// initialize package registry
+//		ArchPackage.eINSTANCE.eClass();
+//		InfrastructurePackage.eINSTANCE.eClass();
+//		RecipesPackage.eINSTANCE.eClass();
+//
+//		// load resource
+//		final ResourceSet rs = new ResourceSetImpl();
+//		final Resource res = rs.getResource(URI.createFileURI(recipeFilename), true);
+//
+//		// traverse model
+//		final EObject o = res.getContents().get(0);
+//
+//		if (o instanceof Configuration) {
+//			
+//		}
+
+	}
+	
+}
