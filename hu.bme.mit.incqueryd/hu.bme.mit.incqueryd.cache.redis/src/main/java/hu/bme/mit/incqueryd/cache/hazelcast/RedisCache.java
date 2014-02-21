4c8bea7ecb23bd9b42be0505749402deccb43369
Dénes Harmath <harmathdenes@gmail.com>
2014-02-21 16:33:56 +0100

    Add Redis-backed cache

---
+++ hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.cache.redis/src/main/java/hu/bme/mit/incqueryd/cache/hazelcast/RedisCache.java
@@ -0,0 +1,21 @@
+package hu.bme.mit.incqueryd.cache.hazelcast;
+
+import hu.bme.mit.incqueryd.cache.DistributedCache;
+import hu.bme.mit.incqueryd.cache.DistributedMultiMap;
+
+import java.util.Collection;
+
+import org.redisson.Config;
+import org.redisson.Redisson;
+
+
+public class RedisCache implements DistributedCache {
+
+	private final Config config = new Config();
+	private final Redisson redisson = Redisson.create(config);
+
+	public <K, V> DistributedMultiMap<K, V> getMultiMap(String name) {
+		return new RedisMultiMap<K, V>(redisson.<K, Collection<V>>getMap(name));
+	}
+
+}
