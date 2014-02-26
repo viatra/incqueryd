64fac07efcbf7fcd2c2a421fef842a04713dd5ff
Gabor Szarnyas <szarnyasg@gmail.com>
2014-02-26 18:30:39 +0100

    fixed RouteSensor query

--- 4s-setup-scripts/4s-start-single.sh
+++ 4s-setup-scripts/4s-start-single.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # kill other running instances
-source 4s-kill.sh
+./4s-kill.sh
 
 export FOURSTORE_CLUSTER_NAME="incqueryd_cluster"
+
+rm -rf /var/lib/4store/$FOURSTORE_CLUSTER_NAME/*
 
 # start new backend
 4s-backend-setup -v $FOURSTORE_CLUSTER_NAME
