54f051ac79b66e180fb615de4a15839be531bb29
Dénes Harmath <harmathdenes@gmail.com>
2014-03-07 16:35:07 +0100

    Add remote actor example

---
+++ hu.bme.mit.incqueryd/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/actors/IncQueryDMicrokernel.java
@@ -0,0 +1,26 @@
+package hu.bme.mit.incqueryd.rete.actors;
+
+import akka.actor.ActorSystem;
+import akka.kernel.Bootable;
+
+import com.typesafe.config.ConfigFactory;
+
+public class IncQueryDMicrokernel implements Bootable {
+
+	public static final String ACTOR_SYSTEM_NAME = "IncQuery-D";
+
+	private ActorSystem system;
+
+	@Override
+	public void startup() {
+		system = ActorSystem.create(ACTOR_SYSTEM_NAME, ConfigFactory.parseString(
+				"akka.actor.provider = akka.remote.RemoteActorRefProvider\n" +
+				"akka.remote.netty.hostname = \"" + System.getenv("localHost") + "\""));
+	}
+
+	@Override
+	public void shutdown() {
+		system.shutdown();
+	}
+
+}
