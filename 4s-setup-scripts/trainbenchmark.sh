3cb24ad629bc7c1e960de13c650708d3bfafe74c
Gabor Szarnyas <szarnyasg@gmail.com>
2014-03-08 01:04:14 +0100

    trainbenchmark.sh fixed

--- 4s-setup-scripts/trainbenchmark.sh
+++ 4s-setup-scripts/trainbenchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./4s-import RDF-models/testBig1.owl
+./4s-import.sh RDF-models/t1.owl
 . trainbenchmark-poslength.sh
 . trainbenchmark-routesensor.sh
 . trainbenchmark-signalneighbor.sh
