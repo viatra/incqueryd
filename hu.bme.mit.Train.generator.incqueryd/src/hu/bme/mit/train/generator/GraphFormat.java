b96bf297ba1ea418fb4806d62415168c2254848c
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-17 14:49:59 +0200

    removed IQD-specific Train Benchmark generator, use the official Train Benchmark implementation instead

--- hu.bme.mit.Train.generator.incqueryd/src/hu/bme/mit/train/generator/GraphFormat.java
+++
@@ -1,7 +0,0 @@
-package hu.bme.mit.train.generator;
-
-public enum GraphFormat {
-	GRAPHML,
-	BLUEPRINTS_GRAPHSON,
-	FAUNUS_GRAPHSON
-}
--- hu.bme.mit.Train.generator.incqueryd/src/hu/bme/mit/train/generator/IGenerator.java
+++
@@ -1,5 +0,0 @@
-package hu.bme.mit.train.generator;
-
-public interface IGenerator {
-	public void testFillUpFile(GeneratorConfig generatorConfig);
-}
