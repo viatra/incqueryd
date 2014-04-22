af8dda8cb6196b181db69ed832cb977aa3282140
Dénes Harmath <harmathdenes@gmail.com>
2014-04-22 14:44:03 +0200

    Add relevant variable references to check expression

--- org.eclipse.incquery.patternlanguage.rdf/org.eclipse.incquery.patternlanguage.rdf/src-gen/org/eclipse/incquery/patternlanguage/rdf/rdfPatternLanguage/RdfCheckConstraint.java
+++ org.eclipse.incquery.patternlanguage.rdf/org.eclipse.incquery.patternlanguage.rdf/src-gen/org/eclipse/incquery/patternlanguage/rdf/rdfPatternLanguage/RdfCheckConstraint.java
@@ -2,6 +2,9 @@
  */
 package org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage;
 
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.incquery.patternlanguage.patternLanguage.VariableReference;
 
 /**
  * <!-- begin-user-doc -->
@@ -11,6 +14,7 @@ package org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage;
  * <p>
  * The following features are supported:
  * <ul>
+ *   <li>{@link org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.RdfCheckConstraint#getVariables <em>Variables</em>}</li>
  *   <li>{@link org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.RdfCheckConstraint#getExpression <em>Expression</em>}</li>
  * </ul>
  * </p>
@@ -21,6 +25,22 @@ package org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage;
  */
 public interface RdfCheckConstraint extends Constraint
 {
+  /**
+   * Returns the value of the '<em><b>Variables</b></em>' containment reference list.
+   * The list contents are of type {@link org.eclipse.incquery.patternlanguage.patternLanguage.VariableReference}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Variables</em>' containment reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Variables</em>' containment reference list.
+   * @see org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.RdfPatternLanguagePackage#getRdfCheckConstraint_Variables()
+   * @model containment="true"
+   * @generated
+   */
+  EList<VariableReference> getVariables();
+
   /**
    * Returns the value of the '<em><b>Expression</b></em>' attribute.
    * <!-- begin-user-doc -->
