afb8d6c4f0d63a90aed9dfa4a84fb749f575a1ea
Dénes Harmath <harmathdenes@gmail.com>
2014-04-28 13:55:16 +0200

    Generalize JSON (de)serializer

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/actors/CoordinatorActor.java
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/actors/CoordinatorActor.java
@@ -7,7 +7,7 @@ import hu.bme.mit.incqueryd.rete.messages.CoordinatorCommand;
 import hu.bme.mit.incqueryd.rete.messages.CoordinatorMessage;
 import hu.bme.mit.incqueryd.rete.messages.Transformation;
 import hu.bme.mit.incqueryd.rete.messages.YellowPages;
-import hu.bme.mit.incqueryd.util.RecipeSerializer;
+import hu.bme.mit.incqueryd.util.EObjectSerializer;
 import hu.bme.mit.incqueryd.util.ReteNodeConfiguration;
 import infrastructure.Machine;
 
@@ -153,7 +153,7 @@ public class CoordinatorActor extends UntypedActor {
 
 				// create a clone, else we would get a java.util.ConcurrentModificationException
 				final ReteNodeRecipe rnrClone = EcoreUtil.copy(rnr);
-				final String recipeString = RecipeSerializer.serializeToString(rnrClone);
+				final String recipeString = EObjectSerializer.serializeToString(rnrClone);
 
 				final Props props;
 				if (remoting) {
