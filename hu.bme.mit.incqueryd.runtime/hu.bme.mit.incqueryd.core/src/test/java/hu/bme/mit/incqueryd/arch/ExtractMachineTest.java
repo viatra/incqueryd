e0dddb07d073fef5d248a8ca79ea728a37805e7f
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-28 13:36:10 +0200

    added incomplete test case for serializing cluster descriptors

---
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/test/java/hu/bme/mit/incqueryd/arch/ExtractMachineTest.java
@@ -0,0 +1,29 @@
+package hu.bme.mit.incqueryd.arch;
+
+import hu.bme.mit.incqueryd.util.RecipeSerializer;
+import infrastructure.Cluster;
+import infrastructure.Machine;
+
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.EList;
+import org.junit.Test;
+
+import arch.Configuration;
+
+public class ExtractMachineTest {
+
+	@Test
+	public void testMachine() throws IOException {
+		final String architectureFile = "src/test/resources/arch/routesensor.arch";
+		final Configuration conf = ArchUtil.loadConfiguration(architectureFile);	
+		
+		final Cluster cluster = conf.getClusters().get(0);
+		final EList<Machine> reteMachines = cluster.getReteMachines();
+		
+		
+		final String clusterJSON = RecipeSerializer.serializeToString(cluster);
+		System.out.println(clusterJSON);
+	}
+	
+}
