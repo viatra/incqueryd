c3d75d78a3a6610856102ae0ad09041c5b5798fc
Dénes Harmath <harmathdenes@gmail.com>
2014-04-28 13:55:16 +0200

    Generalize JSON (de)serializer

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/util/RecipeDeserializer.java
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/util/RecipeDeserializer.java
@@ -1,45 +1,22 @@
 package hu.bme.mit.incqueryd.util;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.commons.io.FileUtils;
-import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.incquery.runtime.rete.recipes.RecipesPackage;
-import org.eclipselabs.emfjson.resource.JsResourceFactoryImpl;
+
+import com.google.common.collect.ImmutableSet;
 
 public class RecipeDeserializer {
 
+	private static final ImmutableSet<RecipesPackage> RECIPE_PACKAGES = ImmutableSet.of(RecipesPackage.eINSTANCE);
+
 	public static EObject deserializeFromFile(final String filename) throws IOException {
-		final ResourceSet resourceSet = new ResourceSetImpl();
-		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("json", new JsResourceFactoryImpl());
-
-		// This line is necessary to register the resource factory.
-		// Without a resource factory, we would get the following exception:
-		// java.lang.RuntimeException: Cannot create a resource for 'http://www.eclipse.org/incquery/rete/recipes'; a
-		// registered resource factory is needed
-		RecipesPackage.eINSTANCE.getReteRecipe();
-
-		final Map<String, Object> options = new HashMap<String, Object>();
-
-		final Resource resource = resourceSet.createResource(URI.createURI(filename));
-		resource.load(options);
-		
-		return resource.getContents().get(0);
+		return EObjectDeserializer.deserializeFromFile(filename, RECIPE_PACKAGES);
 	}
 
 	public static EObject deserializeFromString(final String model) throws IOException {
-		final File tempFile = File.createTempFile("recipe-", ".json");
-		tempFile.deleteOnExit();
-		FileUtils.writeStringToFile(tempFile, model);		
-		
-		return deserializeFromFile(tempFile.getPath());
+		return EObjectDeserializer.deserializeFromString(model, RECIPE_PACKAGES);
 	}
 
 }
