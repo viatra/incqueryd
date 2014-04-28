0d782a8626e759e8a7c67923be0bbae7c4b8a961
Gabor Szarnyas <szarnyasg@gmail.com>
2014-04-28 15:18:38 +0200

    added build-and-deploy script and clean operation to the deploy script

--- hu.bme.mit.incqueryd.runtime/scripts/deploy.sh
+++ hu.bme.mit.incqueryd.runtime/scripts/deploy.sh
@@ -12,6 +12,8 @@ cd "$( cd "$( dirname "$0" )" && pwd )"
 . config.sh
 cd ..
 
+AKKA_DIRECTORY="akka-2.1.4/deploy"
+
 while [ "$1" != "" ]; do
 	case $1 in
 		"--light")
@@ -30,11 +32,14 @@ for machine in ${machines[@]}; do
   rm start-akka.sh
 
   if [ ! $light ]; then
+    # clean the deploy directory
+    ssh $machine "rm $AKKA_DIRECTORY/*"
+
     # third party dependencies
-    scp hu.bme.mit.incqueryd.core/target/lib/* $machine:akka-2.1.4/deploy
+    scp hu.bme.mit.incqueryd.core/target/lib/* $machine:$AKKA_DIRECTORY
   fi
 
   # IncQuery-D's main JAR
-  scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $machine:akka-2.1.4/deploy/
+  scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $machine:$AKKA_DIRECTORY
 done
 
