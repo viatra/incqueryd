7584a38558f613363946a975701bfb7b8e3de72c
Dénes Harmath <harmathdenes@gmail.com>
2014-05-06 16:44:46 +0200

    Finish automatic recipe generator

---
+++ hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf.generator.recipe/src/org/eclipse/incquery/patternlanguage/rdf/generator/recipe/RecipeGeneratorBuilderParticipant.java
@@ -0,0 +1,6 @@
+package org.eclipse.incquery.patternlanguage.rdf.generator.recipe;
+
+import org.eclipse.xtext.builder.BuilderParticipant;
+
+public class RecipeGeneratorBuilderParticipant extends BuilderParticipant {
+}
