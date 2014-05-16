02bd39eadadca74eee78f03544020b0f91d74955
Dénes Harmath <harmathdenes@gmail.com>
2014-05-16 14:31:45 +0200

    Continue implementing RDF pattern matcher context

--- hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf.psystem/xtend-gen/org/eclipse/incquery/patternlanguage/rdf/psystem/RdfPatternMatcherContext.java
+++ hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf.psystem/xtend-gen/org/eclipse/incquery/patternlanguage/rdf/psystem/RdfPatternMatcherContext.java
@@ -1,119 +1,181 @@
 package org.eclipse.incquery.patternlanguage.rdf.psystem;
 
 import java.util.Collection;
+import org.apache.log4j.Logger;
 import org.eclipse.incquery.runtime.matchers.IPatternMatcherContext;
-import org.openrdf.model.Graph;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+import org.openrdf.model.Model;
+import org.openrdf.model.Resource;
+import org.openrdf.model.Statement;
+import org.openrdf.model.Value;
+import org.openrdf.model.vocabulary.RDF;
+import org.openrdf.model.vocabulary.RDFS;
 
 @SuppressWarnings("all")
 public class RdfPatternMatcherContext implements IPatternMatcherContext {
-  private final Graph metamodel;
+  private final Model metamodel;
   
-  public RdfPatternMatcherContext(final Graph metamodel) {
+  private final Logger logger = Logger.getLogger(RdfPatternMatcherContext.class);
+  
+  public RdfPatternMatcherContext(final Model metamodel) {
     this.metamodel = metamodel;
   }
   
-  public IPatternMatcherContext.EdgeInterpretation edgeInterpretation() {
-    return IPatternMatcherContext.EdgeInterpretation.BINARY;
-  }
-  
-  public IPatternMatcherContext.GeneralizationQueryDirection allowedGeneralizationQueryDirection() {
-    return IPatternMatcherContext.GeneralizationQueryDirection.SUPERTYPE_ONLY_SMART_NOTIFICATIONS;
-  }
-  
-  public Object binaryEdgeSourceType(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public Object binaryEdgeTargetType(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public Collection<?> enumerateDirectBinaryEdgeSubtypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public Collection<?> enumerateDirectBinaryEdgeSupertypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public Collection<?> enumerateDirectSubtypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public Collection<?> enumerateDirectSupertypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public Collection<?> enumerateDirectTernaryEdgeSubtypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public Collection<?> enumerateDirectTernaryEdgeSupertypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public boolean isUnaryType(final Object typeObject) {
+    return this.metamodel.contains(((Resource) typeObject), RDF.TYPE, RDFS.CLASS);
   }
   
   public Collection<?> enumerateDirectUnarySubtypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+    Model _filter = this.metamodel.filter(null, RDFS.SUBCLASSOF, ((Resource) typeObject));
+    final Function1<Statement,Resource> _function = new Function1<Statement,Resource>() {
+      public Resource apply(final Statement it) {
+        return it.getSubject();
+      }
+    };
+    Iterable<Resource> _map = IterableExtensions.<Statement, Resource>map(_filter, _function);
+    return IterableExtensions.<Resource>toSet(_map);
   }
   
   public Collection<?> enumerateDirectUnarySupertypes(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public boolean isBinaryEdgeMultiplicityOneTo(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
-  }
-  
-  public boolean isBinaryEdgeMultiplicityToOne(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+    Model _filter = this.metamodel.filter(((Resource) typeObject), RDFS.SUBCLASSOF, null);
+    final Function1<Statement,Value> _function = new Function1<Statement,Value>() {
+      public Value apply(final Statement it) {
+        return it.getObject();
+      }
+    };
+    Iterable<Value> _map = IterableExtensions.<Statement, Value>map(_filter, _function);
+    return IterableExtensions.<Value>toSet(_map);
   }
   
   public boolean isBinaryEdgeType(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+    return false;
   }
   
-  public boolean isTernaryEdgeMultiplicityOneTo(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public Object binaryEdgeSourceType(final Object typeObject) {
+    throw new UnsupportedOperationException();
   }
   
-  public boolean isTernaryEdgeMultiplicityToOne(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public Object binaryEdgeTargetType(final Object typeObject) {
+    throw new UnsupportedOperationException();
+  }
+  
+  public boolean isBinaryEdgeMultiplicityOneTo(final Object typeObject) {
+    throw new UnsupportedOperationException();
+  }
+  
+  public boolean isBinaryEdgeMultiplicityToOne(final Object typeObject) {
+    throw new UnsupportedOperationException();
+  }
+  
+  public Collection<?> enumerateDirectBinaryEdgeSubtypes(final Object typeObject) {
+    throw new UnsupportedOperationException();
+  }
+  
+  public Collection<?> enumerateDirectBinaryEdgeSupertypes(final Object typeObject) {
+    throw new UnsupportedOperationException();
   }
   
   public boolean isTernaryEdgeType(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+    return this.metamodel.contains(((Resource) typeObject), RDF.TYPE, RDF.PROPERTY);
   }
   
-  public boolean isUnaryType(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public Object ternaryEdgeSourceType(final Object typeObject) {
+    Model _filter = this.metamodel.filter(((Resource) typeObject), RDFS.DOMAIN, null);
+    final Function1<Statement,Value> _function = new Function1<Statement,Value>() {
+      public Value apply(final Statement it) {
+        return it.getObject();
+      }
+    };
+    Iterable<Value> _map = IterableExtensions.<Statement, Value>map(_filter, _function);
+    return IterableExtensions.<Value>head(_map);
   }
   
-  public void logDebug(final String message) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public Object ternaryEdgeTargetType(final Object typeObject) {
+    Model _filter = this.metamodel.filter(((Resource) typeObject), RDFS.RANGE, null);
+    final Function1<Statement,Value> _function = new Function1<Statement,Value>() {
+      public Value apply(final Statement it) {
+        return it.getObject();
+      }
+    };
+    Iterable<Value> _map = IterableExtensions.<Statement, Value>map(_filter, _function);
+    return IterableExtensions.<Value>head(_map);
   }
   
-  public void logError(final String message) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public boolean isTernaryEdgeMultiplicityOneTo(final Object typeObject) {
+    return false;
   }
   
-  public void logError(final String message, final Throwable cause) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public boolean isTernaryEdgeMultiplicityToOne(final Object typeObject) {
+    return false;
   }
   
-  public void logFatal(final String message) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public Collection<?> enumerateDirectTernaryEdgeSubtypes(final Object typeObject) {
+    Model _filter = this.metamodel.filter(null, RDFS.SUBPROPERTYOF, ((Resource) typeObject));
+    final Function1<Statement,Resource> _function = new Function1<Statement,Resource>() {
+      public Resource apply(final Statement it) {
+        return it.getSubject();
+      }
+    };
+    Iterable<Resource> _map = IterableExtensions.<Statement, Resource>map(_filter, _function);
+    return IterableExtensions.<Resource>toSet(_map);
   }
   
-  public void logFatal(final String message, final Throwable cause) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public Collection<?> enumerateDirectTernaryEdgeSupertypes(final Object typeObject) {
+    Model _filter = this.metamodel.filter(((Resource) typeObject), RDFS.SUBPROPERTYOF, null);
+    final Function1<Statement,Value> _function = new Function1<Statement,Value>() {
+      public Value apply(final Statement it) {
+        return it.getObject();
+      }
+    };
+    Iterable<Value> _map = IterableExtensions.<Statement, Value>map(_filter, _function);
+    return IterableExtensions.<Value>toSet(_map);
   }
   
-  public void logWarning(final String message) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public IPatternMatcherContext.EdgeInterpretation edgeInterpretation() {
+    return IPatternMatcherContext.EdgeInterpretation.TERNARY;
   }
   
-  public void logWarning(final String message, final Throwable cause) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public IPatternMatcherContext.GeneralizationQueryDirection allowedGeneralizationQueryDirection() {
+    return IPatternMatcherContext.GeneralizationQueryDirection.BOTH;
+  }
+  
+  private final static String INVALID = "Invalid type object";
+  
+  public Collection<?> enumerateDirectSubtypes(final Object typeObject) {
+    Collection<?> _xifexpression = null;
+    boolean _isUnaryType = this.isUnaryType(typeObject);
+    if (_isUnaryType) {
+      _xifexpression = this.enumerateDirectUnarySubtypes(typeObject);
+    } else {
+      Collection<?> _xifexpression_1 = null;
+      boolean _isTernaryEdgeType = this.isTernaryEdgeType(typeObject);
+      if (_isTernaryEdgeType) {
+        _xifexpression_1 = this.enumerateDirectTernaryEdgeSubtypes(typeObject);
+      } else {
+        throw new IllegalArgumentException(RdfPatternMatcherContext.INVALID);
+      }
+      _xifexpression = _xifexpression_1;
+    }
+    return _xifexpression;
+  }
+  
+  public Collection<?> enumerateDirectSupertypes(final Object typeObject) {
+    Collection<?> _xifexpression = null;
+    boolean _isUnaryType = this.isUnaryType(typeObject);
+    if (_isUnaryType) {
+      _xifexpression = this.enumerateDirectUnarySupertypes(typeObject);
+    } else {
+      Collection<?> _xifexpression_1 = null;
+      boolean _isTernaryEdgeType = this.isTernaryEdgeType(typeObject);
+      if (_isTernaryEdgeType) {
+        _xifexpression_1 = this.enumerateDirectTernaryEdgeSupertypes(typeObject);
+      } else {
+        throw new IllegalArgumentException(RdfPatternMatcherContext.INVALID);
+      }
+      _xifexpression = _xifexpression_1;
+    }
+    return _xifexpression;
   }
   
   public String printType(final Object typeObject) {
@@ -121,14 +183,33 @@ public class RdfPatternMatcherContext implements IPatternMatcherContext {
   }
   
   public void reportPatternDependency(final Object pattern) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
   }
   
-  public Object ternaryEdgeSourceType(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public void logDebug(final String message) {
+    this.logger.debug(message);
   }
   
-  public Object ternaryEdgeTargetType(final Object typeObject) {
-    throw new UnsupportedOperationException("TODO: auto-generated method stub");
+  public void logWarning(final String message) {
+    this.logger.warn(message);
+  }
+  
+  public void logWarning(final String message, final Throwable cause) {
+    this.logger.warn(message, cause);
+  }
+  
+  public void logError(final String message) {
+    this.logger.error(message);
+  }
+  
+  public void logError(final String message, final Throwable cause) {
+    this.logger.error(message, cause);
+  }
+  
+  public void logFatal(final String message) {
+    this.logger.fatal(message);
+  }
+  
+  public void logFatal(final String message, final Throwable cause) {
+    this.logger.fatal(message, cause);
   }
 }
