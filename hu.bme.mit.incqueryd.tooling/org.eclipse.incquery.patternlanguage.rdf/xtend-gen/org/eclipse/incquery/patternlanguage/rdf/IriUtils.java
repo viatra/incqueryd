6059cd24f73ad39ed9a27f692ef6960745951552
Dénes Harmath <harmathdenes@gmail.com>
2014-05-17 01:03:52 +0200

    Fix IRI logic

--- hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf/xtend-gen/org/eclipse/incquery/patternlanguage/rdf/IriUtils.java
+++ hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf/xtend-gen/org/eclipse/incquery/patternlanguage/rdf/IriUtils.java
@@ -1,14 +1,44 @@
 package org.eclipse.incquery.patternlanguage.rdf;
 
+import org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.Base;
 import org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.Iri;
 import org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.Prefix;
+import org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.RdfPatternModel;
+import org.eclipse.xtend2.lib.StringConcatenation;
+import org.eclipse.xtext.EcoreUtil2;
 
 @SuppressWarnings("all")
 public class IriUtils {
-  public static String getValue(final Iri it) {
-    Prefix _prefix = it.getPrefix();
-    String _iri = _prefix.getIri();
-    String _iri_1 = it.getIri();
-    return (_iri + _iri_1);
+  public static String getValue(final Iri iri) {
+    String _xblockexpression = null;
+    {
+      String _elvis = null;
+      Prefix _prefix = iri.getPrefix();
+      String _iri = null;
+      if (_prefix!=null) {
+        _iri=_prefix.getIri();
+      }
+      if (_iri != null) {
+        _elvis = _iri;
+      } else {
+        RdfPatternModel _containerOfType = EcoreUtil2.<RdfPatternModel>getContainerOfType(iri, RdfPatternModel.class);
+        Base _base = null;
+        if (_containerOfType!=null) {
+          _base=_containerOfType.getBase();
+        }
+        String _iri_1 = null;
+        if (_base!=null) {
+          _iri_1=_base.getIri();
+        }
+        _elvis = _iri_1;
+      }
+      final String prefix = _elvis;
+      StringConcatenation _builder = new StringConcatenation();
+      _builder.append(prefix, "");
+      String _iri_2 = iri.getIri();
+      _builder.append(_iri_2, "");
+      _xblockexpression = _builder.toString();
+    }
+    return _xblockexpression;
   }
 }
