11c7aa7ee0b3557bc728ca7bb32fa59cb637a9eb
Dénes Harmath <harmathdenes@gmail.com>
2014-05-23 13:44:05 +0200

    Add IRI validation rule

--- hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf/xtend-gen/org/eclipse/incquery/patternlanguage/rdf/validation/RdfPatternLanguageJavaValidator.java
+++ hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf/xtend-gen/org/eclipse/incquery/patternlanguage/rdf/validation/RdfPatternLanguageJavaValidator.java
@@ -6,11 +6,14 @@ import java.net.URL;
 import java.util.Collections;
 import org.eclipse.emf.ecore.EAttribute;
 import org.eclipse.incquery.patternlanguage.patternLanguage.PatternModel;
+import org.eclipse.incquery.patternlanguage.rdf.IriUtils;
+import org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.Iri;
 import org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.RdfPatternLanguagePackage;
 import org.eclipse.incquery.patternlanguage.rdf.rdfPatternLanguage.Vocabulary;
 import org.eclipse.incquery.patternlanguage.rdf.validation.AbstractRdfPatternLanguageJavaValidator;
 import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.xbase.lib.Exceptions;
+import org.openrdf.model.impl.URIImpl;
 
 @SuppressWarnings("all")
 public class RdfPatternLanguageJavaValidator extends AbstractRdfPatternLanguageJavaValidator {
@@ -28,6 +31,22 @@ public class RdfPatternLanguageJavaValidator extends AbstractRdfPatternLanguageJ
         final Exception e = (Exception)_t;
         EAttribute _vocabulary_Location = RdfPatternLanguagePackage.eINSTANCE.getVocabulary_Location();
         this.error("Invalid RDF vocabulary", vocabulary, _vocabulary_Location);
+      } else {
+        throw Exceptions.sneakyThrow(_t);
+      }
+    }
+  }
+  
+  @Check
+  public void checkIri(final Iri iri) {
+    try {
+      String _asString = IriUtils.asString(iri);
+      new URIImpl(_asString);
+    } catch (final Throwable _t) {
+      if (_t instanceof Exception) {
+        final Exception e = (Exception)_t;
+        EAttribute _iri_Value = RdfPatternLanguagePackage.eINSTANCE.getIri_Value();
+        this.error("Invalid IRI, consider specifying a base IRI", iri, _iri_Value);
       } else {
         throw Exceptions.sneakyThrow(_t);
       }
