9efdaae6115951777b6f209cef66f7477b6c19b1
Gabor Szarnyas <szarnyasg@gmail.com>
2014-06-10 16:20:54 +0200

    added Rhino dependency and worked on generalizing the ConditionExpression class -> arbitrary expressions can be evaluated with the TermEvaluatorNode class

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/comparison/ComparisonOperator.java
+++
@@ -1,5 +0,0 @@
-package hu.bme.mit.incqueryd.rete.comparison;
-
-public enum ComparisonOperator {
-    EQUALS, LESS_THAN, LESS_THAN_OR_EQUAL, GREATER_THAN, GREATER_THAN_OR_EQUAL, NOT_EQUALS
-}
--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/comparison/ConditionExpression.java
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/comparison/ConditionExpression.java
@@ -2,27 +2,49 @@ package hu.bme.mit.incqueryd.rete.comparison;
 
 import hu.bme.mit.incqueryd.rete.dataunits.Tuple;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
 /**
  * 
  * @author szarnyasg
+ * @param <V>
  * 
  */
 public class ConditionExpression {
 
-    protected int index;
-    protected ComparisonOperator comparisonOperator;
-    protected long operand;
+	private final String expression;
+	private final Map<String, Integer> parameterIndices;
 
-    public ConditionExpression(final int index, final ComparisonOperator comparisonOperator, final long operand) {
-        this.index = index;
-        this.comparisonOperator = comparisonOperator;
-        this.operand = operand;
-    }
+	public ConditionExpression(final String expression, final Map<String, Integer> parameterIndices) {
+		this.expression = expression;
+		this.parameterIndices = parameterIndices;
+	}
 
-    public boolean satisfiesCondition(final Tuple tuple) {
-//        final Long tupleField = (Long) tuple.get(index);
-    	final Integer tupleField = (Integer) tuple.get(index);
-        return Compare.compare(tupleField, operand, comparisonOperator);
-    }
+	public boolean satisfiesCondition(final Tuple tuple) {
+		// Creates and enters a Context. The Context stores information
+		// about the execution environment of a script.
+		final Context cx = Context.enter();
+		try {
+			// Initialize the standard objects (Object, Function, etc.)
+			// This must be done before scripts can be executed. Returns
+			// a scope object that we use in later calls.
+			final Scriptable scope = cx.initStandardObjects();
+			for (final Entry<String, Integer> entry : parameterIndices.entrySet()) {
+				scope.put(entry.getKey(), scope, tuple.get(entry.getValue()));
+			}
+
+			// Now evaluate the string we've colected.
+			final Object result = cx.evaluateString(scope, expression, "<cmd>", 1, null);
+
+			return (boolean) result;
+
+		} finally {
+			Context.exit();
+		}
+	}
 
 }
