c1a874d862c0d4cf6ca6a760e58f3fb800f33b78
Gabor Szarnyas <szarnyasg@gmail.com>
2014-06-27 13:53:13 +0200

    added scripts, config and patch files for deploying the monitoring for IQD

--- hu.bme.mit.incqueryd.runtime/scripts/deploy.sh
+++ hu.bme.mit.incqueryd.runtime/scripts/deploy.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# Script for deploying the IncQuery-D on the cloud. This includes:
+# Script for deploying IncQuery-D on the cloud. This includes:
 # * light mode
 #   * IncQuery-D core JAR file
 #   * start-akka.sh
-# * full mode
+# * normal mode
 #   * all of the above
 #   * the dependencies of IncQuery-D core
+# * install mode
+#   * Akka microkernel and monitoring components
 
 cd "$( cd "$( dirname "$0" )" && pwd )"
 . config.sh
 cd ..
 
-AKKA_DIRECTORY="akka-2.1.4/deploy"
+AKKA_DEPLOY_DIRECTORY="akka-2.1.4/deploy"
 
 while [ "$1" != "" ]; do
+	case $1 in
+		"--install")
+		install=true
+		;;
+	esac
+	case $1 in
+		"--normal")
+		normal=true
+		;;
+	esac
 	case $1 in
 		"--light")
 		light=true
@@ -26,22 +38,26 @@ done
 for machine in ${machines[@]}; do
 	echo "Deploying IncQuery-D on $machine"
 
-	cd scripts
+	cd scripts/deployable
 	cat start-akka-default.sh | sed "s/export localHost=/export localHost=$machine/" > start-akka.sh
 	chmod +x start-akka.sh
-	scp start-akka.sh start-hazelcast.sh start-server.sh $machine:
+	scp * $machine:
 	rm start-akka.sh
-	cd ..
+	cd ../..
 
-	if [ ! $light ]; then
+	if [ $install ]; then
+		ssh $machine "~/install-akka.sh"
+	fi
+
+	if [[ $normal || $install ]]; then
 		# clean the deploy directory
-		ssh $machine "rm $AKKA_DIRECTORY/*"
+		ssh $machine "rm $AKKA_DEPLOY_DIRECTORY/*"
 
 		# third party dependencies
-		scp hu.bme.mit.incqueryd.core/target/lib/* $machine:$AKKA_DIRECTORY
+		scp hu.bme.mit.incqueryd.core/target/lib/* $machine:$AKKA_DEPLOY_DIRECTORY
 	fi
 
 	# IncQuery-D's main JAR
-	scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $machine:$AKKA_DIRECTORY
+	scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $machine:$AKKA_DEPLOY_DIRECTORY
 done
 
