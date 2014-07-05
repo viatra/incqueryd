e4416cbb3ac6e8833570a29a513569c1ccc59144
Dénes Harmath <harmathdenes@gmail.com>
2014-07-05 09:48:19 +0200

    Execute necessary validations of EMF-IQPL in RDF-IQPL

--- hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf/src/org/eclipse/incquery/patternlanguage/rdf/RdfPatternLanguageRuntimeModule.java
+++ hu.bme.mit.incqueryd.tooling/org.eclipse.incquery.patternlanguage.rdf/src/org/eclipse/incquery/patternlanguage/rdf/RdfPatternLanguageRuntimeModule.java
@@ -5,18 +5,26 @@ package org.eclipse.incquery.patternlanguage.rdf;
 
 import org.eclipse.incquery.patternlanguage.rdf.conversion.RdfPatternLanguageValueConverterService;
 import org.eclipse.xtext.conversion.IValueConverterService;
+import org.eclipse.xtext.xbase.typesystem.legacy.XbaseBatchTypeProvider;
+import org.eclipse.xtext.xbase.typing.ITypeProvider;
+
+import com.google.inject.Binder;
 
 /**
  * Use this class to register components to be used at runtime / without the
  * Equinox extension registry.
  */
-public class RdfPatternLanguageRuntimeModule
-		extends
-		org.eclipse.incquery.patternlanguage.rdf.AbstractRdfPatternLanguageRuntimeModule {
+public class RdfPatternLanguageRuntimeModule extends org.eclipse.incquery.patternlanguage.rdf.AbstractRdfPatternLanguageRuntimeModule {
 
 	@Override
 	public Class<? extends IValueConverterService> bindIValueConverterService() {
 		return RdfPatternLanguageValueConverterService.class;
+	}
+
+	@Override
+	public void configure(Binder binder) {
+		super.configure(binder);
+		binder.bind(ITypeProvider.class).to(XbaseBatchTypeProvider.class);
 	}
 
 }
