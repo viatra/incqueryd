ff9e2ee21a5d07745eb744af736c5795ff1125bf
Gabor Szarnyas <szarnyasg@gmail.com>
2014-07-08 12:22:22 +0200

    renamed TermEvaluatorNode to CheckNode and implemented a simple test case for it

---
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/nodes/CheckNode.java
@@ -0,0 +1,87 @@
+package hu.bme.mit.incqueryd.rete.nodes;
+
+import hu.bme.mit.incqueryd.rete.dataunits.ChangeSet;
+import hu.bme.mit.incqueryd.rete.dataunits.Tuple;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.eclipse.incquery.runtime.rete.recipes.CheckRecipe;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Scriptable;
+
+import com.google.common.collect.Maps;
+
+/**
+ * TermEvaluatorNode [...] deserves special mention because it diverges significantly from the classic RETE concept. It
+ * evaluates a GTASM expression on tuples and filters those tuples for which it evaluates to true. It is similar to an
+ * alpha node, with one key difference: the filtering condition is not required to be constant. The filtering condition
+ * is an arbitrary GTASM term, it is considered as a black box. [Bergmann's MSc thesis, p.41]
+ * 
+ * The current implementation is a simplified version of the one defined above.
+ * 
+ * @author szarnyasg
+ * 
+ */
+public class CheckNode extends AlphaNode {
+
+	private final List<String> expressions = new ArrayList<>();
+	// private final Set<String> inputParameterNames;
+	private final Map<String, Integer> mappedIndices;
+
+	CheckNode(final CheckRecipe recipe) {
+		// TODO use an evaluator shared from runtime
+		for (final Object object : (Object[]) recipe.getExpression().getEvaluator()) {
+			expressions.add((String) object);
+		}
+
+		// converting from EMap to Map
+		mappedIndices = Maps.newHashMap();
+		for (final Entry<String, Integer> entry : recipe.getMappedIndices()) {
+			mappedIndices.put(entry.getKey(), entry.getValue());
+		}
+	}
+
+	@Override
+	public ChangeSet update(final ChangeSet incomingChangeSet) {
+		final Set<Tuple> incomingTuples = incomingChangeSet.getTuples();
+		final Set<Tuple> resultTuples = new HashSet<>();
+
+		for (final Tuple tuple : incomingTuples) {
+			if (satisfiesCondition(tuple)) {
+				resultTuples.add(tuple);
+			}
+		}
+
+		final ChangeSet resultChangeSet = new ChangeSet(resultTuples, incomingChangeSet.getChangeType());
+		return resultChangeSet;
+	}
+
+	protected boolean satisfiesCondition(final Tuple tuple) {
+		final Context context = Context.enter();
+		try {
+			final Scriptable scope = context.initStandardObjects();
+			// for (final String parameterName : inputParameterNames) {
+			
+			boolean satisfies = true;
+			for (final String expression : expressions) {
+
+				for (final String parameterName : mappedIndices.keySet()) {
+					final Integer index = mappedIndices.get(parameterName);
+					final Object value = tuple.get(index);
+					scope.put(parameterName, scope, value);
+				}
+				final Boolean result = (Boolean) context.evaluateString(scope, expression, "<cmd>", 1, null);
+				satisfies &= result;
+			}
+			return satisfies;
+		} finally {
+			Context.exit();
+		}
+	}
+
+}
