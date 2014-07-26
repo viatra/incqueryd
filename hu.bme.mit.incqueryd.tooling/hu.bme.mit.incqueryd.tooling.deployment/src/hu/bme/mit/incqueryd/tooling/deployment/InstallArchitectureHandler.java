fb792ed4e4dfdd70b67915377d789b49f034dc77
Dénes Harmath <harmathdenes@gmail.com>
2014-07-26 21:55:07 +0200

    Start deployment & dashboard buttons

---
+++ hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.tooling.deployment/src/hu/bme/mit/incqueryd/tooling/deployment/InstallArchitectureHandler.java
@@ -0,0 +1,15 @@
+package hu.bme.mit.incqueryd.tooling.deployment;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class InstallArchitectureHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
