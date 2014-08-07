e82a89aea829df45eb24120200d00a74d9c00c31
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-07 19:58:40 +0200

    Deployment Eclipse plug-in and scripts now workings.

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/actors/IncQueryDMicrokernel.java
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/java/hu/bme/mit/incqueryd/rete/actors/IncQueryDMicrokernel.java
@@ -3,8 +3,6 @@ package hu.bme.mit.incqueryd.rete.actors;
 import akka.actor.ActorSystem;
 import akka.kernel.Bootable;
 
-import com.typesafe.config.ConfigFactory;
-
 public class IncQueryDMicrokernel implements Bootable {
 
 	public static final String ACTOR_SYSTEM_NAME = "IncQuery-D";
@@ -13,10 +11,7 @@ public class IncQueryDMicrokernel implements Bootable {
 
 	@Override
 	public void startup() {
-		system = ActorSystem.create(ACTOR_SYSTEM_NAME, ConfigFactory.parseString(
-				"akka.actor.provider = akka.remote.RemoteActorRefProvider\n" +
-				"akka.remote.netty.hostname = \"" + System.getenv("localHost") + "\"\n" + 
-				"akka.remote.netty.message-frame-size = 10000000000"));
+		system = ActorSystem.create(ACTOR_SYSTEM_NAME);
 	}
 
 	@Override
--- hu.bme.mit.incqueryd.runtime/scripts/components/start-akka-default.sh
+++ hu.bme.mit.incqueryd.runtime/scripts/components/start-akka-default.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-export HOSTNAME=<hostname>
 export JAVA_OPTS="-Xms1G -Xmx1G"
+export INSTALL_DIR=~/incqueryd
+export AKKA_VERSION=2.1.4
 
 while [[ ! -z `ps auxw | grep java | grep akka` ]]; do
   echo Waiting for Akka to finish.
   sleep 1
 done
 
-cd
+cd $INSTALL_DIR
 
-rm ~/akka.* 2> /dev/null
+rm akka*.err akka*.out $INSTALL_DIR 2> /dev/null
 
-cd ~/akka-2.1.4
-nohup bin/akka hu.bme.mit.incqueryd.rete.actors.IncQueryDMicrokernel > ~/akka.out 2> ~/akka.err < /dev/null &
+cd akka-$AKKA_VERSION
+
+for port in "$@"
+do
+    echo "$port"
+    nohup bin/akka hu.bme.mit.incqueryd.rete.actors.IncQueryDMicrokernel $port > $INSTALL_DIR/akka-$port.out 2> $INSTALL_DIR/akka-$port.err < /dev/null &
+done
+
