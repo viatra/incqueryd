d1879bd5d6ac5967080ee38e83434602821a49e4
Jozsef Makai <reyuulol@gmail.com>
2014-08-10 01:24:37 +0200

    Implemented HTTP interface for the coordinator actor

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/scala/hu/bme/mit/incqueryd/main/IncQueryDMain.scala
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/scala/hu/bme/mit/incqueryd/main/IncQueryDMain.scala
@@ -3,13 +3,30 @@ package hu.bme.mit.incqueryd.main
 import hu.bme.mit.incqueryd.rete.dataunits.ScalaChangeSet
 import hu.bme.mit.incqueryd.rete.dataunits.Tuple
 import java.util.HashSet
+import akka.actor.ActorSystem
+import akka.actor.Props
+import hu.bme.mit.incqueryd.rete.actors.HttpCoordinatorActorFactory
+import akka.util.Timeout
+import scala.concurrent.duration._
+import akka.io.IO
+import spray.can.Http
+import akka.pattern.ask
 
 object IncQueryDMain {
 
   def main(args: Array[String]) {
     val engine = new IncQueryDEngine
+    
+    val architectureFile = args(0)
+    
+    val monitoringServerIPAddress = if (args.length > 1) args(1) else null
 
-    engine initialize (args(0), true, args(1))
-
+    val coordinator = engine initialize (architectureFile, true, monitoringServerIPAddress)
+    
+    implicit val system = ActorSystem("http")
+    val coordinatorService = system.actorOf(Props(new HttpCoordinatorActorFactory(coordinator)), "coordinator-service")
+    implicit val timeout = Timeout(1000000 seconds)
+    
+    IO(Http) ? Http.Bind(coordinatorService, interface = "localhost", port = 9090)
   }
  }
---
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/scala/hu/bme/mit/incqueryd/rete/actors/HttpCoordinatorActor.scala
@@ -0,0 +1,47 @@
+package hu.bme.mit.incqueryd.rete.actors
+
+import akka.actor.ActorRef
+import akka.actor.Actor
+import akka.util.Timeout
+import akka.pattern.ask
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import spray.can.Http
+import spray.http.HttpRequest
+import spray.http.Uri
+import spray.http._
+import HttpMethods._
+import hu.bme.mit.incqueryd.rete.messages.CoordinatorCommand
+
+class HttpCoordinatorActor (val actorRef: ActorRef) extends Actor {
+  
+  implicit val timeout = Timeout(14400 seconds)
+  
+  def receive = {
+
+    case _: Http.Connected => sender ! Http.Register(self)
+    
+    case HttpRequest(GET, Uri.Path("/start"), _, _, _) => {
+      val future = actorRef ? CoordinatorCommand.START
+      Await.result(future, timeout.duration)
+      
+      sender ! HttpResponse(entity = "DONE")
+    }
+    
+    case HttpRequest(GET, Uri.Path("/transform"), _, _, _) => {
+      val future = actorRef ? CoordinatorCommand.TRANSFORM
+      Await.result(future, timeout.duration)
+      
+      sender ! HttpResponse(entity = "DONE")
+    }
+    
+    case HttpRequest(GET, Uri.Path("/check"), _, _, _) => {
+      val future = actorRef ? CoordinatorCommand.CHECK
+      Await.result(future, timeout.duration)
+      
+      sender ! HttpResponse(entity = "DONE")
+    }
+    
+  }
+  
+}
