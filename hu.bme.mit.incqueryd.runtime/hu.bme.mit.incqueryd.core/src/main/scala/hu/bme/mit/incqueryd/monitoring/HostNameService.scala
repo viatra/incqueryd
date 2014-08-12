829fb0e9772edde68039b46b081295ae09fc8cfb
Jozsef Makai <reyuulol@gmail.com>
2014-08-12 11:52:56 +0200

    Updated ScalaReteActor to give information about its JVM

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/scala/hu/bme/mit/incqueryd/monitoring/HostNameService.scala
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/scala/hu/bme/mit/incqueryd/monitoring/HostNameService.scala
@@ -1,9 +1,12 @@
 package hu.bme.mit.incqueryd.monitoring
 
 import java.net.InetAddress
+import java.lang.management.ManagementFactory
 
 object HostNameService {
   
   val hostName = InetAddress.getLocalHost.getHostName
+  
+  val processName = ManagementFactory.getRuntimeMXBean.getName
   
 }
