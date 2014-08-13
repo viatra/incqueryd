37111f58848a32f4d198adf748a3df131e2324c9
Jozsef Makai <reyuulol@gmail.com>
2014-08-13 17:35:58 +0200

    Start calls coordiantor actor through http and commiting the new engine as well

--- hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.tooling.deployment/src/hu/bme/mit/incqueryd/tooling/deployment/StartArchitectureHandler.java
+++ hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.tooling.deployment/src/hu/bme/mit/incqueryd/tooling/deployment/StartArchitectureHandler.java
@@ -1,14 +1,40 @@
 package hu.bme.mit.incqueryd.tooling.deployment;
 
+import hu.bme.mit.incqueryd.arch.util.ArchUtil;
+import hu.bme.mit.incqueryd.tooling.deployment.httpclient.HttpClient;
+import hu.bme.mit.incqueryd.tooling.deployment.util.ArchitectureSelector;
+
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+
+import arch.Configuration;
 
 public class StartArchitectureHandler extends AbstractHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-		// TODO Auto-generated method stub
+		final IFile file = ArchitectureSelector.getSelection(event);
+		String architectureFile = file.getLocation().toString();
+		Configuration configuration;
+		try {
+			configuration = ArchUtil.loadConfiguration(architectureFile);
+		} catch (IOException e) {
+			throw new ExecutionException("Cannot process architecture file.", e);
+		}
+		
+		String coordinatorIp = configuration.getCoordinatorMachine().getIp();
+		
+		try {
+			String response = HttpClient.getURL("http://" + coordinatorIp +":9090/start");
+			System.out.println(response);
+		} catch (IOException e) {
+			throw new ExecutionException("Coordinator actor hasn't responded.", e);
+		}
+		
 		return null;
 	}
 
---
+++ hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.tooling.deployment/src/hu/bme/mit/incqueryd/tooling/deployment/httpclient/HttpClient.java
@@ -0,0 +1,33 @@
+package hu.bme.mit.incqueryd.tooling.deployment.httpclient;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.apache.commons.io.IOUtils;
+
+public class HttpClient {
+
+	public static String getURL(String urlString) throws IOException {
+		
+		URL url = new URL(urlString);
+
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("GET");
+		conn.setReadTimeout(1000000 * 1000);
+
+		if (conn.getResponseCode() != 200) {
+			throw new IOException(
+					"Failed to get data from service: HTTP error code : "
+							+ conn.getResponseCode());
+		}
+
+		String responseString = IOUtils.toString(conn.getInputStream());
+
+		conn.disconnect();
+
+		return responseString;
+
+	}
+	
+}
