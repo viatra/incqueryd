3385c6eed90c1ac05c3b2f35da9efd907f77e689
Gabor Szarnyas <szarnyasg@gmail.com>
2014-08-13 14:39:59 +0200

    Added coordinator installation to install.sh script.

--- hu.bme.mit.incqueryd.runtime/scripts/install.sh
+++ hu.bme.mit.incqueryd.runtime/scripts/install.sh
@@ -4,7 +4,7 @@
 # * light mode
 #   * IncQuery-D core JAR file
 #   * start-akka.sh
-# * normal mode
+# * full mode (default)
 #   * all of the above
 #   * the dependencies of IncQuery-D core
 #   * Akka microkernel and monitoring components
@@ -13,9 +13,9 @@ cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
 AKKA_VERSION="2.1.4"
 INSTALL_DIR=~/incqueryd/
-AKKA_DEPLOY_DIRECTORY="$INSTALL_DIR/akka-$AKKA_VERSION/deploy/"
+AKKA_DEPLOY_DIR="$INSTALL_DIR/akka-$AKKA_VERSION/deploy/"
 COORDINATOR_INSTALL_DIR="$INSTALL_DIR/coordinator/"
-COORDINATOR_LIB_DIRECTORY="$COORDINATOR_INSTALL_DIR/lib/"
+COORDINATOR_LIB_DIR="$COORDINATOR_INSTALL_DIR/lib/"
 
 case $1 in
 	"--light")
@@ -24,10 +24,13 @@ case $1 in
 	;;
 esac
 
-if [[ $# -eq 0 ]] ; then
-    echo 'Usage: install.sh [--light] <hostname-1> <hostname-2> ...'
+if [[ $# -le 1 ]] ; then
+    echo 'Usage: install.sh [--light] <coordinator-address> <machine-0-address> <machine-1-address> ...'
     exit 0
 fi
+
+coordinator=$1
+shift
 
 # iterating through the hostnames of the machines
 for machine in "$@"; do
@@ -42,30 +45,36 @@ for machine in "$@"; do
 		ssh $machine "$INSTALL_DIR/install-akka.sh"
 
 		# clean the deploy directory
-		ssh $machine "rm $AKKA_DEPLOY_DIRECTORY/*"
+		ssh $machine "rm $AKKA_DEPLOY_DIR/*"
 
 		# third party dependencies
-		scp hu.bme.mit.incqueryd.core/target/lib/* $machine:$AKKA_DEPLOY_DIRECTORY
+		scp hu.bme.mit.incqueryd.core/target/lib/* $machine:$AKKA_DEPLOY_DIR
 	fi
 
 	# IncQuery-D's main JAR
-	scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $machine:$AKKA_DEPLOY_DIRECTORY
+	scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $machine:$AKKA_DEPLOY_DIR
 done
 
-# Installing the Main jar with the cordinator actor to the localhost
-echo "Deploying IncQuery-D Main jar on localhost"
+# deploying to the coordinator
 
-mkdir -p $COORDINATOR_INSTALL_DIR/
-mkdir -p $COORDINATOR_LIB_DIRECTORY/
+echo "Creating directories on the coordinator."
+
+ssh $coordinator "mkdir -p $COORDINATOR_INSTALL_DIR"
+ssh $coordinator "mkdir -p $COORDINATOR_LIB_DIR"
+ssh $coordinator "mkdir -p $COORDINATOR_INSTALL_DIR/arch/"
+ssh $coordinator "mkdir -p $COORDINATOR_INSTALL_DIR/recipes/"
+
+# Installing the Main jar with the cordinator actor to the localhost
+echo "Deploying IncQuery-D Main jar to the coordinator."
 
 if [[ ! $light ]]; then
-	rm -f $COORDINATOR_LIB_DIRECTORY/*
+	rm -f $COORDINATOR_LIB_DIR/*
 	# third party dependencies
-	scp hu.bme.mit.incqueryd.core/target/lib/* localhost:$COORDINATOR_LIB_DIRECTORY
+	scp hu.bme.mit.incqueryd.core/target/lib/* $coordinator:$COORDINATOR_LIB_DIR
 fi
 
-# Copying the coordinator starting script(s) to the localhost's install idrectory
-scp scripts/coordinator/* localhost:$COORDINATOR_INSTALL_DIR
+# Copying the coordinator starting script(s) to the coordinator's install idrectory
+scp scripts/coordinator/* $coordinator:$COORDINATOR_INSTALL_DIR
 
 # IncQuery-D's main JAR
-scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar localhost:$COORDINATOR_INSTALL_DIR
+scp hu.bme.mit.incqueryd.core/target/hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $coordinator:$COORDINATOR_INSTALL_DIR
