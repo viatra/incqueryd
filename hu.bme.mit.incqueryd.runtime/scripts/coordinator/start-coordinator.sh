9dea79351f55373c6bfeb48e4792b6c1b1eee0ef
Jozsef Makai <reyuulol@gmail.com>
2014-08-26 10:58:07 +0200

    Fixed the hard coded jar version part of the start-coordinator script

--- hu.bme.mit.incqueryd.runtime/scripts/coordinator/start-coordinator.sh
+++ hu.bme.mit.incqueryd.runtime/scripts/coordinator/start-coordinator.sh
@@ -10,4 +10,4 @@ cd $COORDINATOR_DIR
 
 rm -f coordinator.err coordinator.out $COORDINATOR_DIR 2> /dev/null
 
-nohup java -jar hu.bme.mit.incqueryd.core-1.0.0-SNAPSHOT.jar $architectureFile $interface $MSIPAddress > $COORDINATOR_DIR/coordinator.out 2> $COORDINATOR_DIR/coordinator.err < /dev/null &
+nohup java -jar hu.bme.mit.incqueryd.core-*-SNAPSHOT.jar $architectureFile $interface $MSIPAddress > $COORDINATOR_DIR/coordinator.out 2> $COORDINATOR_DIR/coordinator.err < /dev/null &
