acf684b2ed31ab0b03d445dc1d92116d417c13da
Gabor Szarnyas <szarnyasg@gmail.com>
2014-09-05 09:05:48 +0200

    Refactored main class to use arguments smarter.

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/scala/hu/bme/mit/incqueryd/main/IncQueryDMain.scala
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.core/src/main/scala/hu/bme/mit/incqueryd/main/IncQueryDMain.scala
@@ -11,17 +11,30 @@ import scala.concurrent.duration._
 import akka.io.IO
 import spray.can.Http
 import akka.pattern.ask
+import org.apache.commons.cli.Options
+import org.apache.commons.cli.PosixParser
+import org.apache.commons.cli.Option
 
 object IncQueryDMain {
 
   def main(args: Array[String]) {
-    
-    val architectureFile = args(0)
-    val interface = args(1)
-    var remoting = !(args.length > 2 && args(2) == "local")
-    
-    val engine = new IncQueryDEngine(interface)
-    
+    val options = new Options;
+	val parser = new PosixParser;
+	
+	val architectureFileOption = new Option("architectureFile", true, "The path to the architecture file.");
+	architectureFileOption.setRequired(true)
+	options.addOption(architectureFileOption)
+	
+	options.addOption(new Option("httpAddress", true, "The address to the HTTP interface, the default value is 127.0.0.1."))
+	options.addOption(new Option("local", false, "Run IQD in local mode"));
+	
+	val cmd = parser.parse(options, args);
+
+	val architectureFile = cmd.getOptionValue("architectureFile");
+	val interface = if (cmd.hasOption("interface")) cmd.getOptionValue("interface") else "127.0.0.1"
+	val remoting = !cmd.hasOption("local");	
+	
+    val engine = new IncQueryDEngine(interface)   
     val coordinator = engine initialize (architectureFile, remoting)
     
     implicit val system = ActorSystem("http")
