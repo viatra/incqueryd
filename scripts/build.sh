1a266edf7876b78bc2cd7cb87939ff02fbb53db8
Gabor Szarnyas <szarnyasg@gmail.com>
2014-09-25 11:19:48 +0200

    Removed tooling from general build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,4 @@
 cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
 hu.bme.mit.incqueryd.arch/scripts/build.sh && \
-hu.bme.mit.incqueryd.runtime/scripts/build.sh && \
-hu.bme.mit.incqueryd.tooling/scripts/build.sh
-
+hu.bme.mit.incqueryd.runtime/scripts/build.sh
