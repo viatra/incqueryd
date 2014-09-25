db8de20ce070e265e771e717b2791da38b3e6bfb
Jozsef Makai <reyuulol@gmail.com>
2014-09-25 22:49:26 +0200

    Now handling UniquenessEnforcer in the allocation

--- hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.tooling.ide/src/hu/bme/mit/incqueryd/tooling/ide/util/ReteAllocator.java
+++ hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.tooling.ide/src/hu/bme/mit/incqueryd/tooling/ide/util/ReteAllocator.java
@@ -1,8 +1,9 @@
 package hu.bme.mit.incqueryd.tooling.ide.util;
 
 import hu.bme.mit.incqueryd.arch.util.ArchUtil;
-import hu.bme.mit.incqueryd.csp.algorithm.data.Container;
-import hu.bme.mit.incqueryd.csp.algorithm.data.ContainerTemplate;
+import hu.bme.mit.incqueryd.csp.algorithm.data.xtend.Container;
+import hu.bme.mit.incqueryd.csp.algorithm.data.xtend.ContainerTemplate;
+import hu.bme.mit.incqueryd.csp.algorithm.data.xtend.Node;
 import infrastructure.InfrastructureFactory;
 import infrastructure.Machine;
 import infrastructure.Process;
@@ -30,6 +31,7 @@ import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.incquery.runtime.rete.recipes.AlphaRecipe;
 import org.eclipse.incquery.runtime.rete.recipes.BetaRecipe;
+import org.eclipse.incquery.runtime.rete.recipes.MultiParentNodeRecipe;
 import org.eclipse.incquery.runtime.rete.recipes.ProductionRecipe;
 import org.eclipse.incquery.runtime.rete.recipes.ReteNodeRecipe;
 import org.eclipse.incquery.runtime.rete.recipes.ReteRecipe;
@@ -45,6 +47,11 @@ public class ReteAllocator {
 	private int[][] overheads;
 	private final List<Container> containers = new ArrayList<>();
 	private final List<ContainerTemplate> containerTemplates = new ArrayList<>();
+	
+	private int[][] edges;
+	private final List<Node> nodes = new ArrayList<>();
+	
+	private final Map<Long, List<ReteNodeRecipe>> processes = new HashMap<>();
 	
 	private boolean optimizeForCost;
 	private String recipeFile;
@@ -63,6 +70,12 @@ public class ReteAllocator {
 		
 		ReteRecipe recipe = ArchUtil.loadRecipe(recipeFile);
 		
+		createProcesses(recipe);
+		
+		createNodesAndEdges(recipe);
+	}
+
+	private void createProcesses(ReteRecipe recipe) {
 		EList<ReteNodeRecipe> recipeNodes = recipe.getRecipeNodes();
 		
 		for (ReteNodeRecipe reteNodeRecipe : recipeNodes) {
@@ -71,7 +84,6 @@ public class ReteAllocator {
 		System.out.println();
 		
 		List<ReteNodeRecipe> remainingReteNodes = new ArrayList<>();
-		Map<Long, List<ReteNodeRecipe>> jvms = new HashMap<>();
 		
 		Set<ReteNodeRecipe> recipesToDelete = new HashSet<>();
 		
@@ -83,7 +95,7 @@ public class ReteAllocator {
 			if(reteNodeRecipe instanceof BetaRecipe || reteNodeRecipe instanceof ProductionRecipe || reteNodeRecipe instanceof TypeInputRecipe) {
 				List<ReteNodeRecipe> nodes = new ArrayList<>();
 				nodes.add(reteNodeRecipe);
-				jvms.put(id_counter, nodes);
+				processes.put(id_counter, nodes);
 				id_counter++;
 			}
 			else {
@@ -97,14 +109,24 @@ public class ReteAllocator {
 		
 		while(!remainingReteNodes.isEmpty()) {
 			for(int i = 0; i < remainingReteNodes.size(); i++) {
-				AlphaRecipe alphaRecipe = (AlphaRecipe)remainingReteNodes.get(i); // it is alpha as all betas were removed in first iteration as they have memory
-				ReteNodeRecipe parent = alphaRecipe.getParent(); // parent of this node
+				ReteNodeRecipe parent = null;
+				ReteNodeRecipe _this = null;
+				_this = remainingReteNodes.get(i);
+				if(_this instanceof AlphaRecipe) {
+					AlphaRecipe alphaRecipe = (AlphaRecipe)_this; // it is alpha as all betas were removed in first iteration as they have memory
+					parent = alphaRecipe.getParent(); // parent of this node
+				}
+				else if(_this instanceof MultiParentNodeRecipe) {
+					MultiParentNodeRecipe multiParentRecipe = (MultiParentNodeRecipe)_this; // it is alpha as all betas were removed in first iteration as they have memory
+					parent = multiParentRecipe.getParents().get(0); // parent of this node
+				}
+				
 				String uriOfParent = ArchUtil.getJsonEObjectUri(parent);
-				for (Long jvmID : jvms.keySet()) {
+				for (Long jvmID : processes.keySet()) {
 					// if its parent was in this jvm then add it to the jvm's 1st place
-					if(uriOfParent.equals(ArchUtil.getJsonEObjectUri(jvms.get(jvmID).get(0)))) {
-						jvms.get(jvmID).add(0, alphaRecipe);
-						recipesToDelete.add(alphaRecipe);
+					if(uriOfParent.equals(ArchUtil.getJsonEObjectUri(processes.get(jvmID).get(0)))) {
+						processes.get(jvmID).add(0, _this);
+						recipesToDelete.add(_this);
 						break;
 					}
 				}
@@ -118,17 +140,73 @@ public class ReteAllocator {
 		}
 		
 		System.out.println();
-		for (Long jvmID : jvms.keySet()) {
+		for (Long jvmID : processes.keySet()) {
 			System.out.println(jvmID + ". jvm:");
-			for (ReteNodeRecipe node : jvms.get(jvmID)) {
+			for (ReteNodeRecipe node : processes.get(jvmID)) {
 				System.out.println(node.getClass().getSimpleName() + " " + ArchUtil.getJsonEObjectUri(node));
 			}
 			System.out.println();
 		}
-		
 	}
 	
 	
+	private void createNodesAndEdges(ReteRecipe recipe) {
+		
+		Set<Long> processSet = processes.keySet();
+		for (Long process : processSet) {
+			Node node = new Node(process.longValue(), process.toString(), 1000);
+			nodes.add(node);
+		}
+		
+		edges = new int[nodes.size()][nodes.size()];
+		for(int i = 0; i < edges.length; i++) {
+			int[] row = edges[i];
+			for(int j = 0; j < row.length; j++) {
+				edges[i][j] = 0;
+			}
+		}
+		
+		EList<ReteNodeRecipe> recipeNodes = recipe.getRecipeNodes();
+		for (ReteNodeRecipe reteNodeRecipe : recipeNodes) {
+			if(reteNodeRecipe instanceof BetaRecipe) {
+				BetaRecipe betaRecipe = (BetaRecipe) reteNodeRecipe;
+				long[] processesOfParents = getProcessesOfParents(betaRecipe.getLeftParent().getParent(),betaRecipe.getRightParent().getParent());
+				for (long l : processesOfParents) {
+					System.out.println(l);
+				}
+			}
+			else if(reteNodeRecipe instanceof AlphaRecipe) {
+				long[] processesOfParents = getProcessesOfParents(((AlphaRecipe) reteNodeRecipe).getParent());
+				for (long l : processesOfParents) {
+					System.out.println(l);
+				}
+			}
+		}
+		
+	}
+	
+	private long[] getProcessesOfParents(ReteNodeRecipe... parents) {
+		long[] parentProcessIds = new long[parents.length];
+		
+		int i = 0;
+		
+		Set<Long> processSet = processes.keySet();
+		for (ReteNodeRecipe parent : parents) {
+			String uriOfParent = ArchUtil.getJsonEObjectUri(parent);
+			for (Long process : processSet) {
+				List<ReteNodeRecipe> nodesInProcess = processes.get(process);
+				for (ReteNodeRecipe reteNodeRecipe : nodesInProcess) {
+					if(uriOfParent.equals(ArchUtil.getJsonEObjectUri(reteNodeRecipe))) {
+						parentProcessIds[i] = process.longValue();
+						i++;
+						break;
+					}
+				}
+			}
+		}
+		
+		return parentProcessIds;
+	}
 	
 	private void processInventory (String inventoryFile) throws IOException {
 		Inventory inventory = ArchUtil.loadInventory(inventoryFile);
