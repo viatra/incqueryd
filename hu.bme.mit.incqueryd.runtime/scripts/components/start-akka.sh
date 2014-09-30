66d5bef06ec7d8f046f5afb80111d29c4bb77989
Jozsef Makai <reyuulol@gmail.com>
2014-09-30 09:53:55 +0200

    Forgot to commit modified start-akka script

--- hu.bme.mit.incqueryd.runtime/scripts/components/start-akka.sh
+++ hu.bme.mit.incqueryd.runtime/scripts/components/start-akka.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-export JAVA_OPTS="-Xms1G -Xmx1G"
+if [[ $# -lt 2 ]] ; then
+    echo 'Usage: start-akka.sh <port> <memorysize>'
+    exit 0
+fi
+
+port=$1
+memory=$2
+
+m="m"
+
+export JAVA_OPTS="-Xms256m -Xmx$memory$m"
 export INSTALL_DIR=~/incqueryd
 export AKKA_VERSION=2.1.4
 
@@ -16,9 +26,7 @@ rm akka*.err akka*.out $INSTALL_DIR 2> /dev/null
 
 cd akka-$AKKA_VERSION
 
-for port in "$@"
-do
-    echo "$port"
-    nohup bin/akka hu.bme.mit.incqueryd.core.rete.actors.IncQueryDMicrokernel $port > $INSTALL_DIR/akka-$port.out 2> $INSTALL_DIR/akka-$port.err < /dev/null &
-done
+echo "$port"
+nohup bin/akka hu.bme.mit.incqueryd.core.rete.actors.IncQueryDMicrokernel $port > $INSTALL_DIR/akka-$port.out 2> $INSTALL_DIR/akka-$port.err < /dev/null &
+
 
