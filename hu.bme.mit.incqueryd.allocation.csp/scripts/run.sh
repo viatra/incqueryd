280df90a227f3d15bed0a104ce2526e14e3e5caa
Gabor Szarnyas <szarnyasg@gmail.com>
2014-10-11 16:26:25 +0200

    Updated help text, added example script

---
+++ hu.bme.mit.incqueryd.allocation.csp/scripts/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd "$( cd "$( dirname "$0" )" && pwd )/.."
+
+java -Djava.library.path=native/linux/x86_64 -cp target/*:target/lib/* hu.bme.mit.incqueryd.csp.main.AllocationMain -recipe /home/szarnyasg/git/incqueryd/hu.bme.mit.incqueryd.queries/recipes/switchSensor.recipe -inventory /home/szarnyasg/git/incqueryd/hu.bme.mit.incqueryd.queries/inventory/Sample.inventory -objectivefunction communication -architecture /tmp/opt.arch -objectivefunction cost
