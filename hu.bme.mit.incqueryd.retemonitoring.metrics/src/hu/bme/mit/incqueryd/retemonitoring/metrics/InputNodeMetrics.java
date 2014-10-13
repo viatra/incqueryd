f3f8c3b6d1df40d1cc0ab38fc3df4d389010a109
Jozsef Makai <reyuulol@gmail.com>
2014-10-13 11:25:40 +0200

    typename for inputndoe

--- hu.bme.mit.incqueryd.retemonitoring.metrics/src/hu/bme/mit/incqueryd/retemonitoring/metrics/InputNodeMetrics.java
+++ hu.bme.mit.incqueryd.retemonitoring.metrics/src/hu/bme/mit/incqueryd/retemonitoring/metrics/InputNodeMetrics.java
@@ -5,9 +5,10 @@ import java.util.List;
 public class InputNodeMetrics extends MemoryNodeMetrics {
 
 	public InputNodeMetrics(String reteNode, String hostName, String processName, String nodeType, String nodeclass,
-			String actorPath, int updateMessagesSent, int changesCount, int tuples, double memory, List<ReteSubscriber> subscribers) {
+			String actorPath, int updateMessagesSent, int changesCount, int tuples, double memory, List<ReteSubscriber> subscribers, String typeName) {
 		super(reteNode, hostName, processName, nodeType, nodeclass, actorPath, updateMessagesSent, changesCount, memory, subscribers);
 		this.tuples = tuples;
+		this.typeName = typeName;
 	}
 
 	/**
@@ -16,9 +17,14 @@ public class InputNodeMetrics extends MemoryNodeMetrics {
 	private static final long serialVersionUID = 1L;
 	
 	private int tuples;
+	private String typeName;
 	
 	public int getTuples() {
 		return tuples;
+	}
+	
+	public String getTypeName() {
+		return typeName;
 	}
 	
 	public String toString() {
