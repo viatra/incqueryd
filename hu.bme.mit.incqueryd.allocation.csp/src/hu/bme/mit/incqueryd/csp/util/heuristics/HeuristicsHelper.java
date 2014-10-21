d2c6abcf8d5718d3ba12d0fd4c6e979158729e59
Jozsef Makai <reyuulol@gmail.com>
2014-10-21 16:45:16 +0200

    More memory

--- hu.bme.mit.incqueryd.allocation.csp/src/hu/bme/mit/incqueryd/csp/util/heuristics/HeuristicsHelper.java
+++ hu.bme.mit.incqueryd.allocation.csp/src/hu/bme/mit/incqueryd/csp/util/heuristics/HeuristicsHelper.java
@@ -5,7 +5,7 @@ public class HeuristicsHelper {
 	public static int getEstimatedMemoryUsage(long normalizedTupleIn, long normalizedTupleOut) {
 		double memorySize = 0.0003 * normalizedTupleIn + 52.969;
 		memorySize += (0.0003 * normalizedTupleOut);
-		int memory = Math.max(128, ((int) Math.ceil(memorySize * 1.4)));
+		int memory = Math.max(128, ((int) Math.ceil(memorySize * 1.5)));
 		return memory;
 	}
 
