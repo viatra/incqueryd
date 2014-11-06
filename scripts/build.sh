860227b5b269a2fa5b39471253596dc2719c9e37
Gabor Szarnyas <szarnyasg@gmail.com>
2014-11-06 16:34:38 +0100

    Updated build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,6 +2,7 @@
 
 cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
+hu.bme.mit.incqueryd.monitoring/scripts/build.sh && \
 hu.bme.mit.incqueryd.arch/scripts/build.sh && \
 hu.bme.mit.incqueryd.allocation.csp/scripts/build.sh && \
 hu.bme.mit.incqueryd.stats/scripts/build.sh && \
