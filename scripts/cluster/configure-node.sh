609a9ee01a009b150f08557e493e48defd7de389
thSoft <harmathdenes@gmail.com>
2014-11-17 08:27:15 -0800

    #7: Start SSH daemon

--- scripts/cluster/configure-node.sh
+++ scripts/cluster/configure-node.sh
@@ -2,6 +2,5 @@ set -e
 : ${1?"Required parameter: list of node IPs (semicolon separated)"}
 sudo apt-get install -y docker.io
 IMAGE=incqueryd/node
-export IPS=$1
 sudo docker build -t=$IMAGE .
-sudo docker run -t -i --net=host $IMAGE /bin/bash
+sudo docker run -e IPS=$1 -d -p 50022:22 $IMAGE /bin/bash -c ./init-container.sh
---
+++ scripts/cluster/init-container.sh
@@ -0,0 +1,2 @@
+4store-graph-driver/scripts/4s-init.sh $IPS
+/usr/sbin/sshd -D
