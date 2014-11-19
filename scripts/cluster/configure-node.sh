cee6392f02a334c239f0bfe4ef8b92753a34c9ac
thSoft <harmathdenes@gmail.com>
2014-11-19 07:38:24 -0800

    Expose more necessary ports

--- scripts/cluster/configure-node.sh
+++ scripts/cluster/configure-node.sh
@@ -3,4 +3,4 @@ set -e
 sudo apt-get install -y docker.io
 IMAGE=incqueryd/node
 sudo docker build -t=$IMAGE .
-sudo docker run -e IPS=$1 -d -p 50022:22 $IMAGE /bin/bash -c ./init-container.sh
+sudo docker run -e IPS=$1 -d -p 50022:22 -p 9090:9090 $IMAGE /bin/bash -c ./init-container.sh
