fbc644a25ca3e20072482ad70c46d4e36918b805
Dénes Harmath <harmathdenes@gmail.com>
2014-11-20 18:31:29 +0100

    Expose & map more ports

--- scripts/cluster/configure-node.sh
+++ scripts/cluster/configure-node.sh
@@ -3,4 +3,4 @@ set -e
 sudo apt-get install -y docker.io
 IMAGE=incqueryd/node
 sudo docker build -t=$IMAGE .
-sudo docker run -e IPS=$1 -d -p 50022:22 -p 9090:9090 $IMAGE /bin/bash -c ./init-container.sh
+sudo docker run -e IPS=$1 -d -p 50022:22 -p 2552:2552 -p 2553:2553 -p 6733:6733 -p 8080:8080 -p 9090:9090 $IMAGE /bin/bash -c ./init-container.sh
