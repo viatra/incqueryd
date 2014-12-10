5be0ac3fcffdb5dca75a5f85aa866f339bdf318b
Dénes Harmath <harmathdenes@gmail.com>
2014-12-10 11:58:07 +0100

    Fix URL

--- hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.bootstrapagent/start.sh
+++ hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.bootstrapagent/start.sh
@@ -1,5 +1,5 @@
 FILE=main.py
-wget https://raw.githubusercontent.com/FTSRG/incqueryd/master/BootstrapAgent/$FILE
+wget https://raw.githubusercontent.com/FTSRG/incqueryd/issues/35/hu.bme.mit.incqueryd.runtime/hu.bme.mit.incqueryd.bootstrapagent/$FILE
 sudo apt-get install python python-setuptools
 sudo easy_install Werkzeug
 python $FILE
