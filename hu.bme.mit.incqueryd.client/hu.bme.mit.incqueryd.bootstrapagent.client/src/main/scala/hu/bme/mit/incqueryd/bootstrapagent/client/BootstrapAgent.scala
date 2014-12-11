01d09bc7d4a098b73c2cf882a3b046b797dfb81d
Dénes Harmath <harmathdenes@gmail.com>
2014-12-11 17:15:19 +0100

    Eliminate duplication

--- hu.bme.mit.incqueryd.client/hu.bme.mit.incqueryd.bootstrapagent.client/src/main/scala/hu/bme/mit/incqueryd/bootstrapagent/client/BootstrapAgent.scala
+++ hu.bme.mit.incqueryd.client/hu.bme.mit.incqueryd.bootstrapagent.client/src/main/scala/hu/bme/mit/incqueryd/bootstrapagent/client/BootstrapAgent.scala
@@ -1,15 +1,14 @@
 package hu.bme.mit.incqueryd.bootstrapagent.client
 
 import scala.collection.JavaConversions.asScalaBuffer
-
 import org.apache.http.HttpStatus
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.DefaultHttpClient
-
 import hu.bme.mit.incqueryd.infrastructureagent.client.InfrastructureAgent
 import hu.bme.mit.incqueryd.inventory.InstanceSet
 import hu.bme.mit.incqueryd.inventory.Inventory
 import hu.bme.mit.incqueryd.inventory.MachineInstance
+import eu.mondo.utils.WebServiceUtils
 
 object BootstrapAgent {
 
@@ -26,14 +25,8 @@ class BootstrapAgent(val instance: MachineInstance) {
 
   def bootstrap: InfrastructureAgent = {
     println(s"Bootstrapping ${instance.getIp}")
-    val httpClient = new DefaultHttpClient
     val port = 4242 // XXX duplication
-    val httpGet = new HttpGet(s"http://${instance.getIp}:$port")
-    val response = httpClient.execute(httpGet)
-    httpGet.releaseConnection
-    if (response.getStatusLine.getStatusCode != HttpStatus.SC_OK) {
-      throw new RuntimeException("Bootstrapping failed, status: " + response.getStatusLine)
-    }
+    WebServiceUtils.call(instance.getIp, port, "")
     new InfrastructureAgent(instance)
   }
 
