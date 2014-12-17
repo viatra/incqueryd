bd5aca0f2767887b5584d90a38411b161538878a
Dénes Harmath <harmathdenes@gmail.com>
2014-12-17 14:04:03 +0100

    Replace waiting for key press with sleeping

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -3,10 +3,9 @@
 set -e
 cd "$( cd "$( dirname "$0" )" && pwd )/.."
 
-mvn clean install
-echo "Press any key when the Infrastructure Agent is ready"
+mvn clean install --offline
 hu.bme.mit.incqueryd.runtime/scripts/start.sh
-read -n 1 -s
+sleep 15s # XXX
 cd hu.bme.mit.incqueryd.test
 mvn verify -Dtest=**/ITDevelopment*
 cd ..
