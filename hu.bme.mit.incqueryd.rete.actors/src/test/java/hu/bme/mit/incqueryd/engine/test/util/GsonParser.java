2b5063a1a600981ab3030ae0ae583377b38c231b
Gabor Szarnyas <szarnyasg@gmail.com>
2015-01-13 14:45:00 +0100

    Actors project working

---
+++ hu.bme.mit.incqueryd.rete.actors/src/test/java/hu/bme/mit/incqueryd/engine/test/util/GsonParser.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2010-2014, Gabor Szarnyas, Istvan Rath and Daniel Varro
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Gabor Szarnyas - initial API and implementation
+ *******************************************************************************/
+package hu.bme.mit.incqueryd.engine.test.util;
+
+import hu.bme.mit.incqueryd.engine.rete.dataunits.Tuple;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class GsonParser {
+
+	public static Gson getGsonParser() {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(Tuple.class, new TupleDeserializer());
+		Gson gson = gsonBuilder.create();
+		return gson;
+	}
+
+}
