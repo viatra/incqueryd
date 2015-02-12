b66fec7fef29d82a34ed0a73d59b31d35fac4c1f
Dénes Harmath <harmathdenes@gmail.com>
2015-02-12 09:55:08 +0100

    Remove unnecessary code

--- hu.bme.mit.incqueryd.client/hu.bme.mit.incqueryd.actorservice/src/main/scala/hu/bme/mit/incqueryd/actorservice/LocalActorService.scala
+++ hu.bme.mit.incqueryd.client/hu.bme.mit.incqueryd.actorservice/src/main/scala/hu/bme/mit/incqueryd/actorservice/LocalActorService.scala
@@ -11,11 +11,7 @@ object LocalActorService extends ActorService {
   override def start(id: ActorId, actorClass: Class[_ <: Actor]): ActorRef = {
     val actorSystem = AkkaUtils.getRemotingActorSystem(id.actorSystemName, id.ip, id.port)
     val props = Props(actorClass)
-    val actor = actorSystem.actorOf(props, id.name)
-    AkkaUtils.retry(10)(1000) {
-      actor ! ""
-    }
-    actor
+    actorSystem.actorOf(props, id.name)
   }
 
 }
