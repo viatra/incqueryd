0fe33ef1ebfbf62c58f820bd978fda51e33c9b80
Dénes Harmath <harmathdenes@gmail.com>
2015-08-24 16:02:59 +0200

    Fix NotSerializableException

--- hu.bme.mit.incqueryd.client/hu.bme.mit.incqueryd.rete.nodes/src/main/java/hu/bme/mit/incqueryd/engine/util/DatabaseConnection.java
+++ hu.bme.mit.incqueryd.client/hu.bme.mit.incqueryd.rete.nodes/src/main/java/hu/bme/mit/incqueryd/engine/util/DatabaseConnection.java
@@ -1,13 +1,14 @@
 package hu.bme.mit.incqueryd.engine.util;
 
-import java.io.IOException;
+import java.io.Serializable;
 
 import eu.mondo.driver.file.FileGraphDriverRead;
 import eu.mondo.driver.fourstore.FourStoreGraphDriverRead;
 import eu.mondo.driver.graph.RDFGraphDriverRead;
 
-public class DatabaseConnection {
+public class DatabaseConnection implements Serializable {
 
+	private static final long serialVersionUID = 1L;
 	private final String connectionString;
 	private final Backend backend;
 
