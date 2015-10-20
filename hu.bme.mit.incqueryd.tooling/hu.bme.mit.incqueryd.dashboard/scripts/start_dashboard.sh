44e1e1f7c9f76b37e6eff8f74555b9c8cd688b4a
Dénes Harmath <harmathdenes@gmail.com>
2015-10-20 12:09:43 +0200

    Don't run dashboard in debug mode

--- hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.dashboard/scripts/start_dashboard.sh
+++ hu.bme.mit.incqueryd.tooling/hu.bme.mit.incqueryd.dashboard/scripts/start_dashboard.sh
@@ -3,4 +3,4 @@
 set -e
 cd "$( cd "$( dirname "$0" )" && pwd )"
 
-java -Xdebug -agentlib:jdwp=transport=dt_socket,address=7779,server=y,suspend=n -jar ../target/hu.bme.mit.incqueryd.dashboard-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+java -jar ../target/hu.bme.mit.incqueryd.dashboard-0.0.1-SNAPSHOT-jar-with-dependencies.jar
